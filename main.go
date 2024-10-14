0fedfd334d4551272b52202da8a67f293ba5ac9f
johnotieno0 <poptiff@gmail.com>
2024-10-14 23:28:40 +0300

    feat: implement bufio scaer for stdin and error handling if input is incorrect

--- main.go
+++ main.go
@@ -1,2 +1,27 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) != 1 {
+		fmt.Println("Usage: go run main.go")
+		return
+	}
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		num, err := strconv.ParseFloat(scanner.Text(), 64)
+		if err != nil {
+			fmt.Printf("Unable to convert %s\n", scanner.Text())
+		}
+		// estimateRange(num)
+		fmt.Println(num)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("error reading input: ", err)
+	}
+}
