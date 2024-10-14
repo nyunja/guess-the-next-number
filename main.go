2e765e727d2d18ed3c888da928c2d17d0cb3b425
johnotieno0 <poptiff@gmail.com>
2024-10-15 00:04:29 +0300

    feat: implement linearRegression to get slope and intercept for the line of best fit

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 )
 
-var data []float64
+var y []float64
 var count int
 
 func main() {
@@ -31,15 +31,36 @@ func main() {
 
 func estimateRange(n float64) {
 	var lower, upper float64
-	if len(data) == 50 {
-		data = data[1:]
+	if len(y) == 50 {
+		y = y[1:]
 	}
-	data = append(data, n)
+	y = append(y, n)
 	if count < 50 {
 		lower = n - 90
 		upper = n + 90
 		count++
+	} else {
+		x := make([]float64, 0, len(y))
+		for i := range y {
+			x[i] = float64(i)
+		}
+		slope, intercept := linearRegression(x, y)
+		fmt.Println(slope, intercept)
+
 	}
 	fmt.Printf("%.2f %.2f\n", lower, upper)
+}
 
+func linearRegression(x, y []float64) (float64, float64) {
+	n := float64(len(x))
+	var sumX, sumXY, sumY, sumX2 float64
+	for i := range x {
+		sumX += x[i]
+		sumY += y[i]
+		sumXY += x[i] * y[i]
+		sumX2 += x[i] * x[i]
+	}
+	slope := (n*sumXY - sumX*sumY) / (n*sumX2 - sumX * sumX)
+	intercept := (sumY - slope*sumX) / n
+	return slope, intercept
 }
